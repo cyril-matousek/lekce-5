1c9d6496ec737d7283b0c7f3018652ac3ac9e387
Cyril Matousek <cyril.matousek@volny.cz>
2014-10-09 16:02:50 +0200

    Pyramida je opravdu pyramida

--- Pyramida.java
+++ Pyramida.java
@@ -4,7 +4,11 @@ public class Pyramida {
 		int vyska = Integer.parseInt(args[0]);
 		
 		for (int y = 0; y < vyska; y++) {
-			for (int x = 0; x < y * 2 + 1; x++) {
+			int sirka = (vyska * 2 - 1);
+			for (int z = (sirka / 2 - y); z > 0; z--) {
+				System.out.print(" ");
+			}  
+			for (int x = 0; x < y * 2 + 1; x++) {				
 				System.out.printf("X");
 			}
 			System.out.println();
